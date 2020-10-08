c840b580c393f21fbd5e1eecca34e88cf9986b33
mikeyangelo5 <71859576+mikeyangelo5@users.noreply.github.com>
2020-10-08 17:53:28 +0700

    Master Commit

--- HelloWorld/app/src/main/java/com/example/helloworld/Homescreen.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/Homescreen.java
@@ -1,6 +1,14 @@
 package com.example.helloworld;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.helloworld;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private EditText inputusername;
+    private EditText inputpassword;
+    private Button loginbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +23,24 @@ public class MainActivity extends AppCompatActivity {
         Log.i(TAG, "OnCreate: Called");
         Log.i(TAG, "OnCreate: Called Two Times");
         setContentView(R.layout.activity_main);
+        inputusername = findViewById(R.id.input_username);
+        inputusername = findViewById(R.id.input_password);
+        loginbutton = findViewById(R.id.button_login);
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonclicked();
+            }
+        });
+    }
+
+    private void buttonclicked(){
+        if(inputusername.getText().toString().equals("admin") && inputpassword.getText().toString().equals("admin")) {
+            Intent homescreen = new Intent(MainActivity.this,Homescreen.class);
+            Toast.makeText(getApplicationContext(), "Welcome " + inputusername.getText(), Toast.LENGTH_LONG).show();
+            startActivity(homescreen);
+        } else {
+            Toast.makeText(getApplicationContext(), "Username atau Password Tidak Benar ", Toast.LENGTH_LONG).show();
+        }
     }
 }
