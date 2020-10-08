cd36bff7ed5d6d96228ca188e03dd72dbfce8ffe
mikeyangelo5 <71859576+mikeyangelo5@users.noreply.github.com>
2020-10-08 17:22:03 +0700

    Master Commit

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.helloworld;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,8 +11,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.i(TAG, "OnCreate: Called");
-        Log.i(TAG, "OnCreate: Called Two Times");
         setContentView(R.layout.activity_main);
     }
 }
