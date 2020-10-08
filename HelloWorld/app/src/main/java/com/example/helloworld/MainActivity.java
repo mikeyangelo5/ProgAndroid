aac470e90b2b3064981d79760d3a9d70760035b5
mikeyangelo5 <71859576+mikeyangelo5@users.noreply.github.com>
2020-10-08 17:02:35 +0700

    Branch Commit

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.helloworld;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -24,5 +27,20 @@ public class MainActivity extends AppCompatActivity {
         usernameinput = findViewById(R.id.input_username);
         passwordinput = findViewById(R.id.input_password);
         loginbutton = findViewById(R.id.button_login);
+        loginbutton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                buttonclicked();
+            }
+        });
+    }
+    private void buttonclicked(){
+        if(usernameinput.getText().toString().equals("admin") && passwordinput.getText().toString().equals("admin")){
+            Toast.makeText(getApplicationContext(),"Welcome " + usernameinput.getText(), Toast.LENGTH_LONG).show();
+            Intent home = new Intent(MainActivity.this,Homescreen.class);
+            startActivity(home);
+        } else {
+            Toast.makeText(getApplicationContext(), "Username Dan Password anda tidak benar", Toast.LENGTH_LONG).show();
+        }
     }
 }
