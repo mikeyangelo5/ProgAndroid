592d564082236fb68e37462af5066d57677f9dbd
mikeyangelo5 <71859576+mikeyangelo5@users.noreply.github.com>
2020-10-08 17:58:34 +0700

    Master Commit

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -13,8 +13,8 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-    private EditText inputusername;
-    private EditText inputpassword;
+    private EditText username;
+    private EditText password;
     private Button loginbutton;
 
     @Override
@@ -23,8 +23,8 @@ public class MainActivity extends AppCompatActivity {
         Log.i(TAG, "OnCreate: Called");
         Log.i(TAG, "OnCreate: Called Two Times");
         setContentView(R.layout.activity_main);
-        inputusername = findViewById(R.id.input_username);
-        inputusername = findViewById(R.id.input_password);
+        username = findViewById(R.id.input_username);
+        password = findViewById(R.id.input_password);
         loginbutton = findViewById(R.id.button_login);
         loginbutton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,9 +35,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void buttonclicked(){
-        if(inputusername.getText().toString().equals("admin") && inputpassword.getText().toString().equals("admin")) {
+        if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")) {
             Intent homescreen = new Intent(MainActivity.this,Homescreen.class);
-            Toast.makeText(getApplicationContext(), "Welcome " + inputusername.getText(), Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Welcome " + username.getText(), Toast.LENGTH_LONG).show();
             startActivity(homescreen);
         } else {
             Toast.makeText(getApplicationContext(), "Username atau Password Tidak Benar ", Toast.LENGTH_LONG).show();
